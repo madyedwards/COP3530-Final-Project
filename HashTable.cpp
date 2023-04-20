2b36b055ce8474f3d1b7bcd5c81109af0b4067da
ariellat16 <86077376+ariellat16@users.noreply.github.com>
2023-04-19 20:54:45 -0400

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -37,3 +37,52 @@ void HashTable::CheckTag(std::string tag) {
     }
 
 }
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left +1;
+    int n2 = right - mid;
+    int X[n1], Y[n2];
+
+    for (int i = 0; i < n1; i++)
+        X[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        Y[j] = arr[mid + 1 + j];
+
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2){
+        if (X[i] <= Y[j]) {
+            arr[k] = X[i];
+            i++;
+        }
+        else {
+            arr[k] = Y[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = X[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = Y[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort (int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) /2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
